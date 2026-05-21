cb53dec53c67e479496445d73e563661d098678f
ItsSexy <arno.joachim@hotmail.com>
2026-05-21 12:16:53 +0200

    Fix | calc, hp banner, responsiveness

--- src/assets/js/components/hp-explore.js
+++ src/assets/js/components/hp-explore.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (!isSmallDevice) {
             // If the device is 768px or larger, fetch all inputs from .hp-explore-list
-            console.log('Device width is 768px or larger. Fetching inputs from .hp-explore-list...');
+            //console.log('Device width is 768px or larger. Fetching inputs from .hp-explore-list...');
             const exploreList = document.querySelector('.hp-explore-list');
             if (exploreList) {
                 const inputs = exploreList.querySelectorAll('.hp-explore-input');
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add an event listener for viewport changes
     window.addEventListener('resize', () => {
-        console.log('Viewport size changed. Re-running the script...');
+        //console.log('Viewport size changed. Re-running the script...');
         runScript(); // Re-run the script when the viewport size changes
     });
 });
